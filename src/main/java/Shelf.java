7d6105283180df7b2effb96c1fe3eabe35977923
kati3hehe <smithkei027@gmail.com>
2025-09-25 15:07:39 -0700

    Generated: Constructor getsets equals & hashMap Created: toString getBookCount *addBook* unfinished

--- src/main/java/Shelf.java
+++ src/main/java/Shelf.java
@@ -1,12 +1,76 @@
+import Utilities.Code;
+
 import java.util.HashMap;
+import java.util.Objects;
 
 public class Shelf {
-    public static final int SHELF_NUMBER_;
-    public static final int SUBJECT_;
+    public static final int SHELF_NUMBER_ = 0;
+    public static final int SUBJECT_ = 1;
 
     private HashMap<Book, Integer> books;
     private int shelfNumber;
     private String subject;
 
+    public Shelf(){
 
+    }
+
+    public Shelf(int shelfNumber, String subject) {
+        this.shelfNumber = shelfNumber;
+        this.subject = subject;
+    }
+
+
+    public Code addBook(Book book) {
+
+    }
+
+
+    public int getBookCount(Book book) {
+        if(!books.containsKey(book)) {
+            return -1;
+        }
+        return books.get(book);
+    }
+
+    public HashMap<Book, Integer> getBooks() {
+        return books;
+    }
+
+    public void setBooks(HashMap<Book, Integer> books) {
+        this.books = books;
+    }
+
+    public int getShelfNumber() {
+        return shelfNumber;
+    }
+
+    public void setShelfNumber(int shelfNumber) {
+        this.shelfNumber = shelfNumber;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Shelf shelf = (Shelf) o;
+        return shelfNumber == shelf.shelfNumber && Objects.equals(subject, shelf.subject);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(shelfNumber, subject);
+    }
+
+    @Override
+    public String toString() {
+        return shelfNumber + " : " + subject;
+    }
 }

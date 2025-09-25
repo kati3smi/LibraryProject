e99c08396b63fa48ff59515caf2a50a942aa6a62
kati3hehe <smithkei027@gmail.com>
2025-09-24 23:54:50 -0700

    Initial commit

---
+++ src/main/java/Shelf.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class Shelf {
+    public static final int SHELF_NUMBER_;
+    public static final int SUBJECT_;
+
+    private HashMap<Book, Integer> books;
+    private int shelfNumber;
+    private String subject;
+
+
+}
